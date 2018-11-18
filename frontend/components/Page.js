fdd855a654134e5803c16240c9e3a34b8a2f338d
matias.kantele <matias.kantele@siili.com>
2018-11-18 02:45:50 +0200

    add global styling

--- frontend/components/Page.js
+++ frontend/components/Page.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 import Meta from "./Meta";
 import Header from "./Header";
-import styled, { ThemeProvider, injectGlobal } from "styled-components";
+import styled, { ThemeProvider, createGlobalStyle } from "styled-components";
 
 const theme = {
   red: "#FF0000",
@@ -25,11 +25,39 @@ const Inner = styled.div`
   padding: 2rem;
 `;
 
+const GlobalStyle = createGlobalStyle`
+  @font-face {
+    font-family: 'radnika_next';
+    src: url('/static/radnikanext-medium-webfont.woff2') format('woff2');
+    font-weight: normal;
+    font-style: normal;
+  }
+  html {
+    box-sizing: border-box;
+    font-size: 10px;
+  }
+  *, *:before, *:after {
+    box-sizing: inherit;
+  }
+  body {
+    padding: 0;
+    margin: 0;
+    font-size: 1.5rem;
+    line-height: 2;
+    font-family: 'radnika_next';
+  }
+  a {
+    text-decoration: none;
+    color: ${props => props.theme.black};
+  }
+`;
+
 class Page extends Component {
   render() {
     return (
       <ThemeProvider theme={theme}>
         <StyledPage>
+          <GlobalStyle />
           <Meta />
           <Header />
           <Inner>{this.props.children}</Inner>
